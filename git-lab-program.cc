1943c0f6e2b8e636edddc5ebef464274cd24d7bb
JacobSipka <js503422@ohio.edu>
2024-01-26 15:26:59 -0500

    Hello Git

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Jacob Sipka
+ *        Date: 1/26/2024
+ * Description: A simple program that prints 'Hello Git'
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+
+    return 0;
+}// main
